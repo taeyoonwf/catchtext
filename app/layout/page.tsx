a447db902f439287fccb8e6ac8f138eff03abb78
taeyoonwf <taeyoonwf@gmail.com>
2024-01-12 10:06:24 -0800

    layout test

---
+++ app/layout/page.tsx
@@ -0,0 +1,43 @@
+"use client"
+import './layout.css';
+
+export default function Home() {
+    return (<body className="">
+        <header className="top-part">A</header>
+        <div className="left-part">
+            <div className='menu'>
+                <button className='menu-btn'><h2>Add Text</h2></button>
+                <button className='menu-btn'><h2>Library</h2></button>
+                <button className='menu-btn'><h2>Quiz</h2></button>
+                <button className='menu-btn'><h2>Dictionary</h2></button>
+                <button className='menu-btn'><h2>Etc</h2></button>
+            </div>
+        </div>
+        <div className="right-part">
+            <h1>C1</h1>
+            <h2>D1</h2>
+            <h3>E1</h3>
+            <h4>F1</h4>
+            <h5>G1</h5>
+
+            <h1>C2</h1>
+            <h2>D2</h2>
+            <h3>E2</h3>
+            <h4>F2</h4>
+            <h5>G2</h5>
+
+            <h1>C3</h1>
+            <h2>D3</h2>
+            <h3>E3</h3>
+            <h4>F3</h4>
+            <h5>G3</h5>
+
+            <h1>C4</h1>
+            <h2>D4</h2>
+            <h3>E4</h3>
+            <h4>F4</h4>
+            <h5>G4</h5>
+        </div>
+    </body>
+    );
+}
