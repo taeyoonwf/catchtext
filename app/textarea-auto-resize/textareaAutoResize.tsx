9b5abcecc00162aaaa3e8d64d5b608bb64112de3
taeyoonwf <taeyoonwf@gmail.com>
2024-02-08 18:51:36 -0800

    fix a minor bug

--- app/textarea-auto-resize/textareaAutoResize.tsx
+++ app/textarea-auto-resize/textareaAutoResize.tsx
@@ -1,7 +1,6 @@
 "use client"
-import React, { ChangeEventHandler, useRef, useState, useEffect } from 'react';
+import React, { ChangeEventHandler, useState } from 'react';
 import './layout.css';
-
 interface TextareaAutoResizeProps {
     className?: string;
     value: string;
@@ -9,22 +8,16 @@ interface TextareaAutoResizeProps {
 }
 
 export default function TextareaAutoResize({className, value, onChange}: TextareaAutoResizeProps) {
-    const divWrapper = useRef<HTMLDivElement>(null);
     const [text, setText] = useState(value);
     
-    useEffect(() => {
-        divWrapper.current!.dataset.replicatedValue = value;
-    }, []);
-
     const handleTextChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
         const { value } = e.currentTarget;
         setText(value);
-        divWrapper.current!.dataset.replicatedValue = value;
-        // onChange?.call(e);
         onChange?.call(null, e);
     };
 
-    return (<div className={`${className !== undefined ? className : ''} grow-wrap`} ref={divWrapper}>
+    return (<div data-replicated-value={text}
+        className={`${className !== undefined ? className : ''} grow-wrap`}>
         <textarea onChange={handleTextChange} value={text}></textarea>
     </div>);
 }
