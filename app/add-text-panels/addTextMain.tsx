eb8ed284ff474ad35107fdf490feec383c97575c
taeyoonwf <taeyoonwf@gmail.com>
2024-03-31 21:42:02 -0700

    implement text selection

--- app/add-text-panels/addTextMain.tsx
+++ app/add-text-panels/addTextMain.tsx
@@ -1,6 +1,6 @@
 'use client'
 import './layout.css';
-import { ChangeEvent, useContext, useState } from 'react';
+import { CSSProperties, ChangeEvent, useContext, useEffect, useRef, useState } from 'react';
 import { Blank, BlankType, DefaultDialect, LangIdType, LangIds, colorSeries } from '../baseTypes';
 import DropdownSelector from '../dropdown-selector/dropdownSelector';
 import { LanguageIdentifierContext } from '../language-identifier/languageIdentifier';
@@ -9,13 +9,171 @@ import TextareaAutoResize from '../textarea-auto-resize/textareaAutoResize';
 import segment from 'sentencex';
 import { LanguageIdentifierResultType } from '../linguaWrapper';
 
+interface divTextArgs {
+  divider: string;
+  sentence: string;
+  divSelColor: string;
+  senSelColor: string;
+} 
+
+enum MoveDivider {
+  NoAction,
+  MoveStarted,
+  CheckedStartIndex,
+}
+let moveDividerStage: MoveDivider = MoveDivider.NoAction;
+
 export default function AddTextMain() {
   const [text, setText] = useState('');
   const [langId, setLangId] = useState<LangIdType|BlankType>(Blank);
   const [langIdOptions, setLangIdOptions] = useState<LangIdType[]>([]);
-  const [divText, setDivText] = useState<JSX.Element[]>([]);
+  const [divText, setDivText] = useState<divTextArgs[]>([]);
   const languageIdentifier = useContext(LanguageIdentifierContext);
   const [textUnitProps, setTextUnitProps] = useState<TextUnitProps>({text: ' '});
+  //const [dividers, setDividers] = useState<string[]>([]);
+  //const [sentences, setSentences] = useState<string[]>([]);
+  //const [selColors, setSelColors] = useState<string[]>([]);
+  //const [selDivTextIndex, setSelDivTextIndex] = useState(-1);
+
+  const selection = useRef('');
+  //let isMovingDivider = false;
+  const [selColor, setSelColor] = useState('#555555');
+        
+  const handleSelection = () => {
+    const sel = document.getSelection();
+    if (sel !== null) {
+      const text = sel.toString();
+      //console.log('in handleSelection');
+      //console.log(text);
+      if (text) {
+        selection.current = text;
+      }  
+    }
+  }
+ 
+  const onAllMouseUp = (e: any) => {
+    if (moveDividerStage == MoveDivider.CheckedStartIndex) {
+      moveDividerDone(e)
+    }
+  }
+
+  useEffect(() => {
+    document.addEventListener('selectionchange', handleSelection);
+    document.addEventListener('mouseup', onAllMouseUp);
+    return () => {
+      document.removeEventListener('selectionchange', handleSelection);
+      document.removeEventListener('mouseup', onAllMouseUp);
+    };
+   });
+
+   //let checkAnchor = false;
+   const moveDividerBegin = (e: React.MouseEvent<HTMLSpanElement, MouseEvent>) => {
+    console.log('moving start');
+    moveDividerStage = MoveDivider.MoveStarted
+    //moveDividerStage = 1;
+    //isMovingDivider = true;
+    //checkAnchor = true;
+  }
+
+  const moveDivider = (e: React.MouseEvent<HTMLSpanElement, MouseEvent>) => {
+    //const theSpan: JSX.Element = e.target;
+    if (e.buttons === 1 && moveDividerStage == MoveDivider.MoveStarted) {
+      console.log("moving divider");
+      const sel = window.getSelection();
+      if (sel !== null) { // && checkAnchor) {
+        console.log(sel.anchorNode);
+        console.log(sel.focusNode);
+        const anchorNodeIndex = Array.prototype.indexOf.call(sel.anchorNode?.parentNode?.parentNode?.childNodes, sel.anchorNode?.parentNode);
+        console.log("anchorNodeIndex : " + anchorNodeIndex);
+  
+        moveDividerStage = MoveDivider.CheckedStartIndex;
+        //checkAnchor = false;
+        //setSelDivTextIndex(Math.floor((anchorNodeIndex + 1) * 0.5));
+        
+        const divIndex = Math.floor((anchorNodeIndex + 1) * 0.5);
+        const colorPrev = colorSeries[(divIndex - 1) % colorSeries.length];
+        const colorNext = colorSeries[divIndex % colorSeries.length];
+        const newDivText: divTextArgs[] = [];
+        for (let i = 0; i < divText.length; i++) {
+          newDivText[i] = {
+            divider: divText[i].divider,
+            sentence: divText[i].sentence,
+            divSelColor: (i < divIndex) ? colorNext : ((i == divIndex) ? 'transparent' : colorPrev),
+            senSelColor: (i < divIndex) ? colorNext : colorPrev,
+          };
+          //divText[i].divSelColor = colorNext;
+          //divText[i].senSelColor = colorNext;
+        }
+        setDivText(newDivText);
+        //divText[divIndex].senSelColor = colorPrev;
+        //for (let i = divIndex + 1; i < divText.length; i++) {
+          //divText[i].divSelColor = colorPrev;
+          //divText[i].senSelColor = colorPrev;
+        //}
+        console.log('anchor check finish ' + moveDividerStage);
+      }
+    }
+    /*else {
+      console.log('moveDividerStage ' + moveDividerStage);
+    }*/
+    //console.log(e.buttons);
+  }
+
+  const moveDividerDone = (e: React.MouseEvent<HTMLSpanElement, MouseEvent>) => {
+    console.log("moving done");
+    //console.log(e.currentTarget);
+    //console.log(window.getSelection());
+    //console.log(window.getSelection());
+    const sel = window.getSelection();
+    console.log(sel);
+    console.log(moveDividerStage);
+    if (sel !== null && moveDividerStage == MoveDivider.CheckedStartIndex) {
+      console.log(sel.anchorNode);
+      console.log(sel.focusNode);
+      const anchorNodeIndex = Array.prototype.indexOf.call(sel.anchorNode?.parentNode?.parentNode?.childNodes, sel.anchorNode?.parentNode);
+      const focusNodeIndex = Array.prototype.indexOf.call(sel.focusNode?.parentNode?.parentNode?.childNodes, sel.focusNode?.parentNode);
+      const divIndex = Math.floor((anchorNodeIndex + 1) * 0.5);
+
+      if (focusNodeIndex % 2 == 0) {
+      }
+      else {
+
+      }
+      console.log(anchorNodeIndex + ", " + focusNodeIndex);
+      const colorIndex = Math.floor((anchorNodeIndex - 1) * 0.5);
+      setSelColor(colorSeries[colorIndex % colorSeries.length]);
+      console.log(colorSeries[colorIndex % colorSeries.length]);
+
+      moveDividerStage = MoveDivider.NoAction
+      const newDivText: divTextArgs[] = [];
+      for (let i = 0; i < divText.length; i++) {
+        newDivText[i] = {
+          divider: divText[i].divider,
+          sentence: divText[i].sentence,
+          divSelColor: 'transparent',
+          senSelColor: 'transparent',
+        };
+      }
+      setDivText(newDivText);
+
+      /*
+      console.log(sel.anchorNode?.nodeValue);
+      console.log(sel.anchorNode?.nodeType);
+      console.log(sel.anchorNode?.nodeName);
+      console.log(sel.anchorNode?.ownerDocument);
+      console.log(sel.anchorNode?.getRootNode());
+      console.log(sel.anchorNode?.parentElement);
+      console.log(sel.anchorNode?.parentNode);
+      console.log(sel.anchorNode?.parentElement?.parentElement);
+      console.log(sel.anchorNode?.parentNode?.parentNode);
+      console.log(sel.anchorNode?.parentElement?.children);
+      */
+    }
+    window.getSelection()?.empty();
+    //isMovingDivider = false;
+    //console.log(window.getSelection().);
+    //e.currentTarget.DOCUMENT_POSITION_PRECEDING
+  }
 
   const refreshSegmentedText = (langId: LangIdType|BlankType, value: string) => {
     const sentences: string[] = segment(langId !== Blank ? langId : 'en', value)
@@ -44,14 +202,24 @@ export default function AddTextMain() {
     //<span style={{backgroundColor: 'pink'}}>Hello!</span>
     //<span className="sentence-divider"> </span>
     
-    const newDivText = [];
+    const newDivText: divTextArgs[] = [];
     for (let i = 0; i < sentences.length; i++) {
-      let whiteSpace = (dividers[i].indexOf(' ') < 0 ? " " + dividers[i] : dividers[i])
-        .replaceAll(' ', '\u00A0');
-      newDivText.push(<span key={i * 2} className={i > 0 ? "sentence-divider" : ""}>{whiteSpace}</span>);
-      const s = sentences[i];
-      const color = colorSeries[i % colorSeries.length];
-      newDivText.push(<span key={i * 2 + 1} style={{backgroundColor: color}}>{s}</span>);
+      //let whiteSpace = (dividers[i].indexOf(' ') < 0 ? " " + dividers[i] : dividers[i])
+      //  .replaceAll(' ', '\u00A0');
+      //newDivText.push(<span onMouseDown={moveDividerBegin} onMouseUp={moveDividerDone} onMouseMove={moveDivider}
+      //  key={i * 2} className={i > 0 ? "sentence-divider" : ""}>
+      //    {whiteSpace}
+      //</span>);
+      //const s = sentences[i];
+      //const color = colorSeries[i % colorSeries.length];
+      //newDivText.push(<span onMouseUp={moveDividerDone} onMouseMove={moveDivider}
+      //  key={i * 2 + 1} style={{...{"--selection-color": selColor} as React.CSSProperties, backgroundColor: color}}>{s}</span>);
+      newDivText.push({
+        divider: dividers[i],
+        sentence: sentences[i],
+        divSelColor: 'transparent',
+        senSelColor: 'transparent',
+      });
     }
     setDivText(newDivText);
 
@@ -155,7 +323,26 @@ export default function AddTextMain() {
         </div>
 
         <div className='sentence-segments'>
-          {divText.length > 0 && divText}
+          {divText.map((s, idx) => {
+      //const color = colorSeries[i % colorSeries.length];
+      //newDivText.push(<span onMouseUp={moveDividerDone} onMouseMove={moveDivider}
+      //  key={i * 2 + 1} style={{...{"--selection-color": selColor} as React.CSSProperties, backgroundColor: color}}>{s}</span>);
+
+            return (<>
+              <span onMouseDown={moveDividerBegin} onMouseMove={moveDivider} key={idx * 2}
+                style={{...{"--selection-color": s.divSelColor} as CSSProperties}}
+                className={idx > 0 ? "sentence-divider" : ""}>
+                  {(s.divider.indexOf(' ') < 0 ? " " + s.divider : s.divider)
+                    .replaceAll(' ', '\u00A0')
+                  }
+              </span>
+              <span onMouseMove={moveDivider} key={idx * 2 + 1}
+                style={{...{"--selection-color": s.senSelColor} as CSSProperties,
+                backgroundColor: colorSeries[idx % colorSeries.length]}}>
+                  {s.sentence}
+              </span>
+            </>);
+          })}
         </div>
 
         <TextUnit
@@ -170,5 +357,4 @@ export default function AddTextMain() {
     </div>
   );
 }
-
 
