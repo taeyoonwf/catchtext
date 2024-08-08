b08799d0486d537b467f0d360c6aa98e426c894b
taeyoonwf <taeyoonwf@gmail.com>
2024-08-08 15:55:54 +0900

    linguaWrapper bugfix completed

--- app/linguaWrapper.ts
+++ app/linguaWrapper.ts
@@ -30,15 +30,11 @@ const load: LoadFunctionType = async (input: LanguageIdentifierInputType) => {
   if (!detector && input.init) {
     detector = await fetchData();
   }
+
   if (detector && !input.init) {
     return {
       id: input.id,
-      result: detector.computeLanguageConfidenceValues(input.text
-        .replaceAll(/’/, "'")
-        .replaceAll('—', "-")
-        .replaceAll('“', '"')
-        .replaceAll('”', '"')
-      )
+      result: detector.computeLanguageConfidenceValues(input.text)
     } as LanguageIdentifierOutputType;
   }
   return true;
